e7650947158f20d8d2a88f54eb29cb53293a7cfd
PROPIETARIO <PROPIETARIO@x>
2021-01-31 14:11:26 +0100

    Mensaje de la rama cambios.

--- actividad1/src/actividad1/cambio3.java
+++ actividad1/src/actividad1/cambio3.java
@@ -5,7 +5,7 @@ public class cambio3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Estamos en cambio3 en la rama cambios");
+		System.out.println("Estamos en cambio3 en rama cambios");
 
 	}
 
